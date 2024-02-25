4e4adf98be8dac076ad1188daeb9394956ecb881
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-02-25 20:01:00 +0530

    Add functionality to switch colors
    Add a hidden color picker which opens up on a button click to let user
    choose custom colors. When having random colors (rainbow) the colors
    kept flickering, so had to keep track of the last pixel which was
    colored.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 const canvas = document.querySelector(".canvas");
 const clearBtn = document.querySelector(".clear");
+const eraserBtn = document.querySelector(".eraser");
+const defaultColorPalette = document.querySelector(".default-colors");
+const colorPicker = document.querySelector("input[type='color']");
+const colorSelectorBtn = document.querySelector(".color-picker");
+const customColorsBtn = document.querySelector(".custom-colors-btn")
+
+let isSelectingColor = false;
 const DIMENSION = 21;
+
+let pixels;
+let currentColor = "black";
+
+const defaultColors = ["#333333", "#ff9a51", "#f8fb6a", "#86e67e", "#31c6ed", "#FF51E3", "#9714ff", "rainbow"];
 
 function generateBoard(dimension) {
     for (let i = 0; i < dimension; i++) {
@@ -18,6 +30,32 @@ function generateBoard(dimension) {
     }
 }
 
+function generateDefaultColors() {
+    let colorIndex = 0;
+    for (let i = 0; i < 2; i++) {
+        const colorRow = document.createElement("div");
+        colorRow.classList.add("color-row");
+        for (let j = 0; j < defaultColors.length / 2; j++) {
+            const colorDiv = document.createElement("div");
+            colorDiv.classList.add("color-select");
+            if (defaultColors[colorIndex] === "rainbow") {
+                colorDiv.classList.add("rainbow");
+                colorDiv.addEventListener("click", () => {
+                    currentColor = "rainbow";
+                })
+            }
+            else {
+                colorDiv.style.backgroundColor = defaultColors[colorIndex++];
+                colorDiv.addEventListener("click", () => {
+                    currentColor = colorDiv.style.backgroundColor;
+                })
+            }
+            colorRow.appendChild(colorDiv);
+        }
+        defaultColorPalette.appendChild(colorRow);
+    }
+}
+
 
 function repaintCanvas(pixelColor) {
     Array.from(pixels).forEach(pixel => {
@@ -26,33 +64,77 @@ function repaintCanvas(pixelColor) {
 }
 
 function getAllPixels() {
-    return document.querySelectorAll(".pixel");
+    pixels = document.querySelectorAll(".pixel");
 }
 
+let lastPixel;
 function colorPixel(pixel) {
-    pixel.target.style.backgroundColor = "red";
+    if (currentColor === "rainbow") {
+        const randomIndex = Math.floor(Math.random() * defaultColors.length);
+
+        // Adding this logic, so the colors don't flicker in one pixel
+        if (lastPixel != pixel.target) {
+            pixel.target.style.backgroundColor = defaultColors[randomIndex];
+            lastPixel = pixel.target;
+        }
+    }
+    else {
+        pixel.target.style.backgroundColor = currentColor;
+    }
 }
 
-function captureEvents(pixels) {
+function captureEvents() {
     window.addEventListener("mousedown", () => {
         Array.from(pixels).forEach(pixel => {
             pixel.addEventListener("mousemove", colorPixel)
         })
     })
 
-
     window.addEventListener("mouseup", () => {
         Array.from(pixels).forEach(pixel => {
             pixel.removeEventListener("mousemove", colorPixel)
         })
     })
+
+    Array.from(pixels).forEach(pixel => {
+        pixel.addEventListener("click", () => {
+            if (isSelectingColor) {
+                currentColor = pixel.style.backgroundColor;
+                isSelectingColor = false;
+                document.body.style.cursor = "auto";
+            }
+        })
+    })
+
 }
 
-clearBtn.addEventListener("click", () => {
-    repaintCanvas("white");
-})
+function captureUtilityEvents() {
+    clearBtn.addEventListener("click", () => {
+        repaintCanvas("white");
+    })
+
+    eraserBtn.addEventListener("click", () => {
+        currentColor = "white";
+    })
+
+    colorPicker.addEventListener("change", () => {
+        currentColor = colorPicker.value;
+    })
+
+    customColorsBtn.addEventListener("click", () => {
+        colorPicker.click();
+    })
+
+    colorSelectorBtn.addEventListener("click", () => {
+        document.body.style.cursor = "crosshair";
+        isSelectingColor = true;
+    })
+}
+
 
 
 generateBoard(DIMENSION);
-const pixels = getAllPixels();
-captureEvents(pixels);
+generateDefaultColors();
+getAllPixels();
+captureEvents();
+captureUtilityEvents();
